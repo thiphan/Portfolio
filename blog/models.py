cf8af45e8425b5e66c169bae559240e165e1db7d
thiphan <thoaptk07@gmail.com>
2019-03-05 16:54:17 -0600

    fix blog model bug

--- blog/models.py
+++ blog/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class Blog(models.Model):
     title = models.CharField(max_length=255)
     pub_date = models.DateTimeField()
-    body = models.TextFiled()
+    body = models.TextField()
     image = models.ImageField(upload_to='image/')
